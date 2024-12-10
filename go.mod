11e2339697450a013c79eddbdbf9fccfff2375be
Ivan Mishin <imishin@samokat.ru>
2024-12-10 10:06:26 +0300

    user created with hashed pass

--- go.mod
+++ go.mod
@@ -16,8 +16,9 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/term v0.24.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/crypto v0.30.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/term v0.27.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 )
--- internal/client/signup/call.go
+++ internal/client/signup/call.go
@@ -2,15 +2,19 @@ package signup
 
 import (
 	"context"
+	"secret_keeper/internal/client/encrypt"
 	"secret_keeper/internal/client/logger"
 )
 
 func (i *Item) Call(f Form) error {
 	// TODO подумать, откуда притащить контекст
+	hashedPass, err := encrypt.HashPassword(f.Password)
+	if err != nil {
+		logger.Logger.Error("Error when hash password", "err", err)
+		return err
+	}
 
-	logger.Logger.Info("CReate method")
-
-	err := i.storage.UserCreate(context.Background(), f.Login, f.Password)
+	err = i.storage.UserCreate(context.Background(), f.Login, hashedPass)
 	if err != nil {
 		logger.Logger.Error("Error when create user", "err", err)
 		return err
