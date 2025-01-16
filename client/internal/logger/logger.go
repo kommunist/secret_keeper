b3d3dfe8cbf4052edd6abdf489a40a27526d9d5b
Ivan Mishin <imishin@samokat.ru>
2025-01-16 08:22:45 +0300

    fix logger error

--- client/internal/logger/logger.go
+++ client/internal/logger/logger.go
@@ -11,7 +11,10 @@ var Logger = Make()
 
 // Данный логгер переопределяет основной вывод в файл, так как терминал занят отображением
 func Make() *slog.Logger {
-	file, _ := os.OpenFile(logFile, os.O_RDWR|os.O_APPEND, 0644)
+	file, err := os.OpenFile(logFile, os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err) // Кажется, паниковать в данном случае допустимо. Тем более логгер глобальный
+	}
 	logger := slog.New(slog.NewTextHandler(file, nil))
 	return logger
 }
