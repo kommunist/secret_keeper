2f3f07e96f890012c12dd46ef53c7cc56fc030ca
Ivan Mishin <imishin@samokat.ru>
2025-01-16 08:19:22 +0300

    encryption test

--- client/internal/encrypt/encrypt_test.go
+++ client/internal/encrypt/encrypt_test.go
@@ -11,17 +11,13 @@ func TestHashAndCheck(t *testing.T) {
 
 	password := "password"
 	encrypted, err := HashPassword(password)
+	assert.NoError(t, err, "hashed_password_without_error")
 
 	t.Run("it_correct_encrypt_and_check_password", func(t *testing.T) {
-		assert.NoError(t, err, "hashed_password_without_error")
-
 		assert.True(t, CheckPasswordHash(password, encrypted))
-
 	})
 
 	t.Run("it_correce_encrypt_and_check_password_when_password_incorrect", func(t *testing.T) {
-		assert.NoError(t, err, "hashed_password_without_error")
-
 		assert.False(t, CheckPasswordHash(strings.ToUpper(password), encrypted))
 	})
 
